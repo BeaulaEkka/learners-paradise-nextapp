934a9c394a8494cda075b76417dce152c2df1f2b
Your Name <ekka.beaula@gmail.com>
2025-01-01 23:56:06 +0100

    sonner-installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+
 export default function Home() {
   return (
     <div className="nondashboard-layout">
---
+++ src/components/NonDashboardNavbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NonDashboardNavbar = () => {
+  return (
+    <nav className='nondashboard-layout'>
+
+      navbar
+    </nav>
+  )
+}
+
+export default NonDashboardNavbar

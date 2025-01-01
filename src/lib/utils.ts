8f99d5ffbe1a7c80c0c625a15b34aaa614c5aaee
Your Name <ekka.beaula@gmail.com>
2025-01-01 21:39:24 +0100

    installed shadcn and components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 import * as z from "zod";
 import { api } from "../state/api";
 import { toast } from "sonner";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 // Convert cents to formatted currency string (e.g., 4999 -> "$49.99")
